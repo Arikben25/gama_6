9cddb848af0dd35e3cafee32a6210d50f927285b
Yossi Ben Haim <v0585204770@gmail.com>
2025-05-04 14:35:35 +0300

    added_len_of_word

--- app.py
+++ app.py
@@ -1,10 +1,2 @@
 with open("my_book.txt","r") as f:
     text = f.read()
-
-keys_by_words = {}
-for i in text.split():
-    if i not in keys_by_words:
-        keys_by_words[i] = 1
-    else:
-        keys_by_words[i] += 1
-print(keys_by_words)
