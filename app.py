bdccc98bee5594226c37faf58ed26f68934bfc6e
ariel ben pazi <rbenpazi@gmail.com>
2025-05-04 14:42:43 +0300

    added_sum_of_different_word

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ with open("my_book.txt","r") as f:
     text = f.read()
 
 keys_by_words = {}
-for i in text.split():
-    if i not in keys_by_words:
-        keys_by_words[i] = 1
+for word in text.split():
+    word = word.replace(",","").replace(".","").lower()
+    if word not in keys_by_words:
+        keys_by_words[word] = 1
     else:
-        keys_by_words[i] += 1
-print(keys_by_words)
+        keys_by_words[word] += 1
+
+def sum_of_the_different_words():
+    return len(keys_by_words)
+
+
